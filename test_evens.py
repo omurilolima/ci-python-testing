55b5512784b0c696fa50d6569f300548fb3d9625
Murilo Lima <msl@cesar.school>
2023-03-10 19:22:04 +0000

    Setting up

---
+++ test_evens.py
@@ -0,0 +1,9 @@
+import unittest  # is part of the Python standard library
+from evens import even_numbers_of_evens
+
+class TestEvens(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
