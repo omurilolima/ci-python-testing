f9eb2b660f2d222d4eda0ec75430cbf63f6869dc
Murilo Lima <msl@cesar.school>
2023-03-13 17:15:23 +0000

    Add student.py file

---
+++ student.py
@@ -0,0 +1,15 @@
+from datatime import data, timedelta
+
+class Student:
+    """A Student calss as base for method testing"""
+
+    def __init__(self, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self.end_date = date.today() + timedelta(days=365)
+        self.naughty_list = False
+
+    @property
+    del full_name(self):
+        return f"{self._first_name} {self._last_name}"
