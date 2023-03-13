a3f98507f037e534207a2bd26efb9134795e2781
Murilo Lima <msl@cesar.school>
2023-03-13 19:54:52 +0000

    Add mock method for testing API request

--- test_student.py
+++ test_student.py
@@ -1,6 +1,8 @@
 import unittest
 from student import Student
 from datetime import timedelta
+import requests
+from unittest.mock import patch
 
 
 class TestStudent(unittest.TestCase):
@@ -39,6 +41,21 @@ class TestStudent(unittest.TestCase):
         self.student.apply_extension(5)
 
         self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = 'Success'
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, 'Success')
+
+    def test_course_schedule_failed(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request!")
 
 if __name__ == '__main__':
     unittest.main()
